b08075f198043bae8662ad26025a52ae87895248
DS\KumarSud <skumar@cvrreg.com>
2017-05-15 00:06:37 -0700

    angular sample buildup

---
+++ js/1.BasicAngular.js
@@ -0,0 +1,10 @@
+ angular.module("myApp", [])
+               .controller("ToggleController", function($scope) {
+                   //Properties
+                $scope.visible = true; //default visibility
+
+                //methods
+                $scope.toggle = function() {
+                                    $scope.visible = !$scope.visible;    
+                                };      
+         });
