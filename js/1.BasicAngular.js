705f4504fa7d0ed2993bdccc429d864f4041d18a
DS\KumarSud <skumar@cvrreg.com>
2017-05-19 10:59:18 -0700

    samples

--- js/1.BasicAngular.js
+++ js/1.BasicAngular.js
@@ -1,10 +1,9 @@
  angular.module("myApp", [])
-               .controller("ToggleController", function($scope) {
-                   //Properties
-                $scope.visible = true; //default visibility
-
-                //methods
-                $scope.toggle = function() {
-                                    $scope.visible = !$scope.visible;    
+        .controller("ToggleController", function($scope) {
+              //Properties
+              $scope.visible = true; //default visibility
+              //methods
+              $scope.toggle = function() {
+                                        $scope.visible = !$scope.visible;    
                                 };      
          });
